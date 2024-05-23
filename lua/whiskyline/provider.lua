aeddaf5b39b1d6e6e4e12c50788fde727a4ae8a1
glepnir <glephunter@gmail.com>
2024-05-23 20:21:32 +0800

    fix progress end condition

--- lua/whiskyline/provider.lua
+++ lua/whiskyline/provider.lua
@@ -96,7 +96,7 @@ function pd.progress()
     stl = function(args)
       if args.data and args.data.params then
         local val = args.data.params.value
-        if val.message or val.kind ~= 'end' then
+        if val.message and val.kind ~= 'end' then
           idx = idx + 1 > #spinner and 1 or idx + 1
           return spinner[idx - 1 > 0 and idx - 1 or 1]
         end
