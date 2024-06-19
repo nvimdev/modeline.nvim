ccdfc4b38e3247ee6987b4b5d23e3743dc22a4a2
glepnir <glephunter@gmail.com>
2024-06-19 15:56:27 +0800

    update

--- lua/modeline/provider.lua
+++ lua/modeline/provider.lua
@@ -183,7 +183,7 @@ function pd.gitinfo(git_t)
     stl = gitsigns_data(git_t),
     name = 'git' .. git_t,
     event = { 'User GitSignsUpdate', 'BufEnter' },
-    attr = git_t ~= 'head' and stl_attr('Diff' .. alias[git_t]) or nil,
+    attr = git_t ~= 'head' and stl_attr('GitSigns' .. alias[git_t]) or nil,
   }
 end
 
