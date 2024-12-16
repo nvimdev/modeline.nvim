177347cb35cc9a69fdf867fb70f2cc16d2cd486c
glepnir <glephunter@gmail.com>
2024-12-16 17:39:54 +0800

    update

--- lua/modeline/init.lua
+++ lua/modeline/init.lua
@@ -12,7 +12,7 @@ local function default()
     p.eol(),
     [[%{(&modified&&&readonly?'%*':(&modified?'**':(&readonly?'%%':'--')))}-T%{tabpagenr()}  ]],
     p.fileinfo(),
-    '    %P (%l,%c) ',
+    '    %P (%l,%c)    ',
     p.gitinfo(),
     ' %=',
     [[%{(bufname() !=# '' && &bt != 'terminal' ? '(' : '')}]],
