33f9aaca68f022af78284bffa903c27fb9f238ca
glepnir <glephunter@gmail.com>
2026-01-02 12:37:06 +0800

    update

--- lua/modeline/init.lua
+++ lua/modeline/init.lua
@@ -12,7 +12,7 @@ local function default()
     "%{&encoding=='utf-8'?'U':''}",
     "%{&fileformat=='dos'?'\\\\':&fileformat=='mac'?'/':':'}",
     "%{&readonly?(&modified?'%*':'%%'):(&modified?'**':'--')}-",
-    '  T%{tabpagenr()} %t',
+    '  T%{tabpagenr()} %#StatusLineFile#%t%*',
     "  %P  %{printf('L%-5d', line('.'))}    ",
     p.gitinfo(),
     ' %=',
